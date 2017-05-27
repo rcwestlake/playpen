889d780fe290fe2ed0c636032729e3b7bd28838c
Ryan Westlake <ryancwestlake@gmail.com>
2017-05-27 14:02:34 -0600

    empty promise error

--- jukebox/your-stuff/index.js
+++ jukebox/your-stuff/index.js
@@ -1,5 +1,19 @@
-import Database from '../my-stuff/super-real-totally-not-fake-database';
+import Database from '../my-stuff/super-real-totally-not-fake-database'
 
-export default () => {
-  // Your implementation goes here.
-};
+const getMusicData = async () => {
+  const groups = await Database.findAllArtists()
+
+  for (let artist of groups) {
+    const albums = await Promise.all(artist.albums.map(album => Database.findAlbum(album))) // get albums for each artist
+    artist.albums = albums
+    for (let album of artist.albums) {
+      const songs = await Promise.all(album.songs.map(song => Database.findSong(song))) // get songs for each album
+      album.songs = songs
+    }
+    console.log(artist)
+  }
+
+  return { groups } // wrap it in main object
+}
+
+export default getMusicData
