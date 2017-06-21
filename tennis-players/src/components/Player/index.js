6881f68a1a66d3b3d5ef63059d62c2ab44a17412
Ryan Westlake <ryancwestlake@gmail.com>
2017-06-20 22:40:48 -0600

    create card component

--- tennis-players/src/components/Player/index.js
+++ tennis-players/src/components/Player/index.js
@@ -6,7 +6,21 @@ const Player = (props) => {
   console.log('props', props)
   return (
     <div className={cn(css.column)}>
-      {props.name}
+      <div className={cn(css.card)}>
+        <div className={cn(css['card-image'])}>
+          <figure className={cn(css.image, css["is-480x480"])}>
+            <img src="http://bulma.io/images/placeholders/256x256.png" />
+          </figure>
+        </div>
+        <div className={cn(css['card-content'])}>
+          <div className={cn(css.media)}>
+            <h3 className={cn(css['media-left'], css.title, css['is-4'])}>{props.name}</h3>
+          </div>
+          <div>
+            <h4>Number of wins: {props.number_of_wins}</h4>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
