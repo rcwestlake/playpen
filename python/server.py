c1aa938c8f4814075211ef642511eb9dafe7e0ed
Ryan Westlake <ryancwestlake@gmail.com>
2017-10-01 18:06:54 -0600

    create python server with flask

--- python/server.py
+++ python/server.py
@@ -1,34 +1,33 @@
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-import SocketServer
+from flask import Flask, jsonify, abort, request
 
-class S(BaseHTTPRequestHandler):
-    def _set_headers(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
+app = Flask(__name__)
 
-    def do_GET(self):
-        self._set_headers()
-        self.wfile.write("<html><body><h1>hi!</h1></body></html>")
+tasks = [
+    {
+        "title": "do laundry",
+        "time": 1,
+        "time_type": "hour"
+    },
+    {
+        "title": "send emails",
+        "time": 30,
+        "time_type": "minutes" 
+    }
+]
 
-    def do_HEAD(self):
-        self._set_headers()
-        
-    def do_POST(self):
-        # Doesn't do anything with posted data
-        self._set_headers()
-        self.wfile.write("<html><body><h1>POST!</h1></body></html>")
-        
-def run(server_class=HTTPServer, handler_class=S, port=80):
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    print 'Starting httpd...'
-    httpd.serve_forever()
+@app.route('/api/tasks',  methods=['GET'])
+def get_tasks():
+    return jsonify(tasks)
 
-if __name__ == "__main__":
-    from sys import argv
-
-    if len(argv) == 2:
-        run(port=int(argv[1]))
-    else:
-        run()
+@app.route('/api/tasks', methods=['POST'])
+def add_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'title': request.json['title'],
+        'time': request.json['time'],
+        'time_type': request.json['time_type']
+    }
+    print request.json
+    tasks.append(task)
+    return jsonify({ 'tasks': tasks }), 201
