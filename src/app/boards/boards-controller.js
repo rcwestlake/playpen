f07b8aea86fae9847dd1d6c97b5710eb9c6c3fc1
Ryan Westlake <ryancwestlake@gmail.com>
2017-11-26 00:38:56 -0700

    add boards

--- src/app/boards/boards-controller.js
+++ src/app/boards/boards-controller.js
@@ -1,8 +1,10 @@
 angular.module('noterious')
   .controller('BoardsCtrl', function(BoardsModel) {
     var bctrl = this
-
-    var newBoard = {
+    bctrl.isEditing = false
+    bctrl.loading = false
+    bctrl.newBoard = {
+      id: null,
       title: '',
       description: '',
       isPublic: false
@@ -12,14 +14,63 @@ angular.module('noterious')
       {
         id: 1,
         title: 'test',
-        description: 'do things'
+        description: 'do things',
+        isPublic: true
       },
       {
-        id: 2, 
+        id: 2,
         title: 'biz ideas',
-        description: 'do bizzy things'
+        description: 'do bizzy things',
+        isPublic: false
       }
     ]
 
-    console.log(bctrl.boards)
+    bctrl.createBoard = function(board, isValid) {
+      if(isValid) {
+        board['id'] = bctrl.boards.length + 1
+        bctrl.boards.push(board)
+      }
+      // TODO: send error message if it is not valid
+      bctrl.resetBoard()
+      console.log('boards: ', bctrl.boards)
+    }
+
+    bctrl.setEditBoard = function(boardId, board) {
+      bctrl.editBoardId = boardId
+      bctrl.editBoard = angular.copy(board)
+      bctrl.isEditing = true
+    }
+
+    bctrl.updateBoard = function(boardId, board, isValid) {
+      // NOTE: this mutates data - would want a different solution
+      if(isValid) {
+        var result = bctrl.boards.map(function(el, i) {
+          if(el.id === boardId) {
+            el = board
+          }
+          return el
+        })
+        bctrl.boards = result
+        bctrl.cancelEditing()
+      }
+    }
+
+    bctrl.cancelEditing = function() {
+      bctrl.isEditing = false
+      bctrl.editBoard = {
+        id: null,
+        title: '',
+        description: '',
+        isPublic: false
+      }
+    }
+
+    bctrl.resetBoard = function() {
+      bctrl.newBoard = {
+        id: null,
+        title: '',
+        description: '',
+        isPublic: false
+      }
+    }
   })
